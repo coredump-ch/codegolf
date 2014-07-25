fa720d19bd1dd5579d0f97b9d7d914fb0205b845
Danilo Bargen <mail@dbrgn.ch>
2014-07-25 15:40:27 +0200

    Increased memory limit (32 MB)

--- codegolf/server.py
+++ codegolf/server.py
@@ -121,7 +121,7 @@ def asm_compass_verify(filepath):
                 working_dir='/home/compass/codegolf',
                 volumes=['/code'],
                 network_disabled=True,
-                mem_limit=1024 * 1024,  # 1 MB
+                mem_limit=1024 * 1024 * 32,  # 32 MB
     )
     client.start(cid, binds={dirname: '/code'})
 
